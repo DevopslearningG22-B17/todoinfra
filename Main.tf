08a938de6e7b0d978696e4d7f762eb1db57faa75
sharadsingh31 <sharadsingh31@gmail.com>
2025-07-17 13:32:49 +0530

    Added code

---
+++ Main.tf
@@ -0,0 +1,17 @@
+
+resource "azurerm_resource_group" "sharadrg"{
+  name = var.resource_group_name
+  location = var.location
+}
+
+
+resource "azurerm_storage_account" "sharadstorage" {
+name = var.storage
+  resource_group_name = azurerm_resource_group.sharadrg.name
+  location = var.location
+  account_replication_type = var.account_replication_type
+  account_tier = var.account_tier
+  
+}
+
+
---
+++ Variable.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name"{
+    default = "sharadrg1"
+}
+
+variable "location" {
+    default = "East US"
+}
+
+variable "storage" {
+    default = "sharadstorage1"
+}
+  
+variable "account_tier" {
+    default = "Standard"
+}
+
+ variable "account_replication_type" {
+    default = "GRS"
+ }  
+
