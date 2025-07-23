dae02b364530308d47f5bc616ec32575e374835d
sharadsingh31 <sharadsingh31@gmail.com>
2025-07-23 23:50:16 +0530

    Update Variable.tf (#4)

--- Variable.tf
+++ Variable.tf
@@ -1,17 +1,21 @@
 variable "resource_group_name"{
     default = "sharadrg1"
+type = string
 }
 
 variable "location" {
     default = "East US"
+type = string
 }
 
 variable "storage" {
     default = "sharadstorage1"
+type = string
 }
   
 variable "account_tier" {
     default = "Standard"
+type = string
 }
 
  variable "account_replication_type" {
